303be5d57507d2433a1ab54d2684fb4afbed4625
Awdhesh Kumar <awdheshkumarab98@gmail.com>
2022-11-08 13:31:36 +0530

    Locking database

--- server/models/database.js
+++ server/models/database.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-const db_url = "mongodb+srv://loginform:ktsBWwMWicUPo5Fv@cluster0.530oa.mongodb.net/food?retryWrites=true&w=majority";
+const db_url = "mongodb+srv://loginform:(database link)@cluster0.530oa.mongodb.net/(database_Name)?retryWrites=true&w=majority";
 mongoose.connect(db_url,{useNewUrlParser:true}).then(()=>{ console.log("Database connection established");})
 
 require('./Category');
